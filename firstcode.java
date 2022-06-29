c69aad6cbaa90d49596e9abce929ef582d96c2ca
JuanPabloZ <juanpablo.zunigah@gmail.com>
2022-06-29 17:22:29 +0200

    update

---
+++ firstcode.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] arr = {3, 2, 3, 1, 2};
+    int n = arr.length;  
+    int temp = 0;
+    for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }       
+                 }  
+         }
+  }
+}
